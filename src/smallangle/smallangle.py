e696e1b3a82ddadd3a9ac6f39891de068d20a145
Juaopteinde <juaopteinde@Outlook.com>
2024-11-15 13:07:57 +0100

    Added CML commands

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,15 +1,24 @@
 import click
 import numpy as np
-from numpy import pi
 import pandas as pd
+from numpy import pi
 
 
+@click.group()
+def smallangle():
+    pass
+
+
+@smallangle.command()
+@click.option("-n", "--number", default=10)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 
+@smallangle.command()
+@click.option("-n", "--number", default=10)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -17,4 +26,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    smallangle()

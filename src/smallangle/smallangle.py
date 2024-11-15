810caec1573854caceb3abc4c196eed5ab1bc035
Juaopteinde <juaopteinde@Outlook.com>
2024-11-15 13:59:45 +0100

    Updated docstrings and added comments

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -4,16 +4,19 @@ import pandas as pd
 from numpy import pi
 
 
+# create smallangle command
 @click.group()
 def smallangle():
     pass
 
 
+# create subcommand sin of smallangle
 @smallangle.command()
 @click.option("-n", "--number", default=10)
 def sin(number):
     """Calculate sin(x) and small angle approximation of sin(x)
 
+    \b
     Args:
         number (int): amount of equally spaced numbers 0 < n < 2pi to calculate for
     """
@@ -22,11 +25,13 @@ def sin(number):
     print(df)
 
 
+# create subcommand tan of smallangle
 @smallangle.command()
 @click.option("-n", "--number", default=10)
 def tan(number):
     """Calculate tan(x) and small angle approximation of tan(x)
 
+    \b
     Args:
         number (int): amount of equally spaced numbers 0 < n < 2pi to calculate for
     """
@@ -35,6 +40,7 @@ def tan(number):
     print(df)
 
 
+# create approx command
 @click.command()
 @click.option("-e", "--epsilon", default=0.1)
 @click.option("-t", "--tan", is_flag=True, default=False)
@@ -42,6 +48,7 @@ def tan(number):
 def approx(epsilon, tan, sin):
     """Print the maximum angle for which the small angle of sin(x) and/or tan(x) holds within given accuracy.
 
+    \b
     Args:
         epsilon (str): accuracy whithin which approx must hold
         tan (bool): choose if you want to calculate only for tan(x)
@@ -73,13 +80,13 @@ def approx(epsilon, tan, sin):
         max_angle_sin = 0
         while np.abs(max_angle_sin - np.sin(max_angle_sin)) <= float(epsilon):
             max_angle_sin += 0.001
-        print(
-            f"For an accuracy of {epsilon}, the small-angle approximation for sin(x) holds \n up to x = {round(max_angle_sin, 3)}"
-        )
         max_angle_tan = 0
         while np.abs(max_angle_tan - np.tan(max_angle_tan)) <= float(epsilon):
             max_angle_tan += 0.001
 
+        print(
+            f"For an accuracy of {epsilon}, the small-angle approximation for sin(x) holds \n up to x = {round(max_angle_sin, 3)}"
+        )
         print(
             f"For an accuracy of {epsilon}, the small-angle approximation for tan(x) holds \n up to x = {round(max_angle_tan, 3)}"
         )
